a691ae14b0154bcc2851f0ef0cc422bd3668659b
Matthew Saul <matthewsaul@Matthews-MacBook-Pro.local>
2015-07-18 19:49:06 -0700

    Made a few changes to router.js

--- router.js
+++ router.js
@@ -1,11 +1,13 @@
 var Profile = require("./profile.js");
 var renderer = require("./render.js");
 
+var commonHeaders = {'Content-Type': 'text/plain'};
+
 function home (request, response) {
   //if url == "/" && GET
   if(request.url === "/") {
     //show search
-    response.writeHead(200, {'Content-Type': 'text/plain'}); 
+    response.writeHead(200, commonHeaders); 
     renderer.view("header", {}, response); 
     renderer.view("search", {}, response);
     renderer.view("footer", {}, response);
@@ -20,7 +22,7 @@ function user (request, response) {
   //if url == "/...."
   var username = request.url.replace("/", "");
     if (username.length > 0) {
-      response.writeHead(200, {'Content-Type': 'text/plain'});  
+      response.writeHead(200, commonHeaders);  
       renderer.view("header", {}, response);
       // get json data from Treehouse
     var studentProfile = new Profile(username);
